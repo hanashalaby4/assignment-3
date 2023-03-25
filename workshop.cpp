98d6e2686e40965c261786a36608d7b6ab91744e
hanashalaby4 <shalaby04@aucegypt.edu>
2023-03-25 17:46:04 +0200

    peek function not working

--- workshop.cpp
+++ workshop.cpp
@@ -20,15 +20,20 @@ int main()
 	customer customers[MAX_CUSTOMERS];
 	customers[0].setName("Ahmed");
 	customers[0].setApt({ 1, 0 });
+	customers[0].setID(1);
 	customers[1].setName("Sara");
 	customers[1].setApt({ 4, 0 });
+	customers[1].setID(2);
 	customers[2].setName("Kareem");
 	customers[2].setApt({ 3, 0 });
+	customers[2].setID(3);
 	customers[3].setName("Mohammed");
 	customers[3].setApt({ 1, 0 });
-	
-	Queue<customer> qCustomers(MAX_CUSTOMERS);
+	customers[3].setID(4);
 
+
+	Queue<customer> qCustomers(MAX_CUSTOMERS);
+	//checking availability
 	for (int j = 0; j < MAX_CUSTOMERS; j++)
 	{
 		bool foundMech = false;
@@ -38,15 +43,35 @@ int main()
 			{
 				customers[j].setmID(i + 1);
 				mechanics[i].setApoint(customers[j].getApt());
-				qCustomers.push(customers[j]);
 				foundMech = true;
-				break;			
+				break;		
 			}
 		}
 		if (!foundMech)
 			cout << "No mechanics" << endl;
 	}
+	//swap
+	for (int i = 0; i < MAX_CUSTOMERS; i++)
+	{
+		for (int j = i; j < MAX_MECHANICS; j++)
+		{
+			if (customers[i] > customers[j])
+			{
+				customer temp = customers[i];
+				customers[i] = customers[j];
+				customers[j] = temp;
+			}
+		}
+	}
+	//pushing into queue
+	for (int i = 0; i < MAX_CUSTOMERS; i++)
+	{
+		qCustomers.push(customers[i]);
+	}
 
-
+	for (int i = 0; i < MAX_CUSTOMERS; i++)
+	{
+		cout << qCustomers.peek();
+	}
 
 }
